68fea9101d59aacc50a7c978c6c08773e00564cb
Python-Learn <panchaldhruv878@gmail.com>
2024-12-24 21:39:09 +0530

    Implement the Remove and Exit button

--- gui.py
+++ gui.py
@@ -15,11 +15,13 @@ layout = [[label], [input_box, add_button], [list_box, edit_button, remove_butto
 window = sg.Window("My To-Do App",
                    layout=layout,
                    font=('Helvetica', 15))
+
 while True:
     event, values = window.read()
     print(1, event)
     print(2, values)
     print(3, values["todos"])
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -27,6 +29,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
         case "Edit":
             todo_to_edit = values["todos"][0]
             new_todo = values["todo"]
@@ -35,6 +38,7 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
         case "Remove":
             todo_to_remove = values["todos"][0]
             todos = functions.get_todos()
@@ -42,12 +46,16 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
             window["todo"].update(value="")
+
         case "todos":
             window['todo'].update(value=values['todos'][0])
+
         case "Exit":
             break
+
         case sg.WIN_CLOSED:
             break
             # exit()
             # exit() function stop the execution completely
+
 window.close()
