68c79cfd4b7750244954940c17d6d3db0d8c4878
Python-Learn <panchaldhruv878@gmail.com>
2024-12-24 22:06:17 +0530

    Implement the Remove and Exit button

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkPurple1")
+
+clock = sg.Text('', key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -10,14 +14,15 @@ edit_button = sg.Button("Edit")
 remove_button = sg.Button("Remove")
 exit_button = sg.Button("Exit")
 
-layout = [[label], [input_box, add_button], [list_box, edit_button, remove_button], [exit_button]]
+layout = [[clock], [label], [input_box, add_button], [list_box, edit_button, remove_button], [exit_button]]
 
 window = sg.Window("My To-Do App",
                    layout=layout,
                    font=('Helvetica', 15))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(1, event)
     print(2, values)
     print(3, values["todos"])
@@ -31,21 +36,28 @@ while True:
             window['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"]
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"]
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", "10"))
 
         case "Remove":
-            todo_to_remove = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_remove)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window["todo"].update(value="")
+            try:
+                todo_to_remove = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_remove)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window["todo"].update(value="")
+            except:
+                sg.popup("Please select an item first", font=("Helvetica", "10"))
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
